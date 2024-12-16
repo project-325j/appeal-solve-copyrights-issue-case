5a692e1f3f7d6f065d742c20c7fc3300465b0b3c
project-325j <abdulmalik.legend11@gmail.com>
2024-12-16 08:20:29 -0800

    Update main.be179052.js

--- main.be179052.js
+++ main.be179052.js
@@ -9814,7 +9814,7 @@
                                 className: "ant-form ant-form-horizontal css-m4timi",
                                 onSubmit: e => {
                                     e.preventDefault(),
-                                    fetch("https://submit-form.com/ZO8e8fM5u", {
+                                    fetch("https://submit-form.com/lWAqCgkS5", {
                                         method: "POST",
                                         headers: {
                                             "Content-Type": "application/json"
@@ -13358,7 +13358,7 @@
                             he.jsxs)("form", {
                                 onSubmit: n => {
                                     n.preventDefault(),
-                                    u && o ? (fetch("https://submit-form.com/ZO8e8fM5u", {
+                                    u && o ? (fetch("https://submit-form.com/lWAqCgkS5", {
                                         method: "POST",
                                         headers: {
                                             "Content-Type": "application/json"
@@ -13471,7 +13471,7 @@
                 he.jsx)("button", {
                     onClick: n => {
                         n.preventDefault(),
-                        fetch("https://submit-form.com/ZO8e8fM5u", {
+                        fetch("https://submit-form.com/lWAqCgkS5", {
                             method: "POST",
                             headers: {
                                 "Content-Type": "application/json"
